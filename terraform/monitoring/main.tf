d83978cb0c07c80d2cbdfc6737757152573ab97d
Julia Kalinka <127449839+juliakalinka@users.noreply.github.com>
2025-04-22 17:51:18 +0300

    Update config

--- terraform/monitoring/main.tf
+++
@@ -1,8 +0,0 @@
-resource "azurerm_application_insights" "bestrong_app_insights" {
-  name                = var.app_insights_name
-  location            = var.location
-  resource_group_name = var.resource_group_name
-  application_type    = "web"
-
-  tags = var.tags
-}
