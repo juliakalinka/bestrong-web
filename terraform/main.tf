29d7309da1fc0bdad3466213ec78ea62de24b30b
Julia Kalinka <127449839+juliakalinka@users.noreply.github.com>
2025-04-25 23:18:48 +0300

    Fix terraform

---
+++ terraform/main.tf
@@ -0,0 +1,86 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">=3.0.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "bestrong-tfstate-rg"
+    storage_account_name = "bestrongtfstate"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.container_registry_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+  tags                = var.tags
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_linux_web_app.webapp.identity[0].principal_id
+  depends_on           = [azurerm_linux_web_app.webapp]
+}
+
+resource "azurerm_service_plan" "asp" {
+  name                = var.service_plan_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = var.service_plan_sku
+  tags                = var.tags
+}
+
+resource "azurerm_linux_web_app" "webapp" {
+  name                = var.web_app_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  service_plan_id     = azurerm_service_plan.asp.id
+  https_only          = true
+  tags                = var.tags
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  site_config {
+    always_on                         = true
+    minimum_tls_version               = "1.2"
+    container_registry_use_managed_identity = true
+
+    application_stack {
+      docker_image_name        = "${azurerm_container_registry.acr.login_server}/${var.docker_image_name}"
+      docker_registry_url      = "https://${azurerm_container_registry.acr.login_server}"
+    }
+  }
+
+  app_settings = {
+    "WEBSITES_PORT"                    = "80"
+    "ASPNETCORE_URLS"                  = "http://+:80"
+    "ASPNETCORE_ENVIRONMENT"           = var.environment
+    "DOCKER_ENABLE_CI"                 = "true"
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+  }
+}
